b7f215779adfa9019131954cfd327c5db293fda9
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2023-01-25 22:08:00 +0000

    User model created

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import {register} from "./controllers/auth.js"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -34,6 +35,10 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage})
+
+/* ROUTES WITH FILES */
+
+app.post("auth/register", upload.single("picture"), register)
 
 /* MONGOOSE SETUP */
 
