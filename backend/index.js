a320ff84ba6537c4f61402d1f507821c77c00dec
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2023-01-26 15:50:09 +0000

    can now create a user

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express()
 app.use(express.json())
 app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy)
+app.use(helmet.permittedCrossDomainPolicies())
 app.use(morgan("common"))
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -36,16 +36,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage})
 
-/* ROUTES WITH FILES */
-
-app.post("auth/register", upload.single("picture"), register)
-
 /* MONGOOSE SETUP */
 
-const PORT = process.env.port || 6001
+const PORT = process.env.PORT || 6001
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server port: ${PORT}`))
 }).catch((error) => console.log(`${error} did not connect`));
+
+/* ROUTES WITH FILES */
+
+app.post("/auth/register", upload.single("picture"), register)
